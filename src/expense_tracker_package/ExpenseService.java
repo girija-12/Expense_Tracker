a329fcd2b30737791724e7fc68a7f1b191fd9d1b
Girija <ramagirija95@gmail.com>
2024-09-13 18:02:27 +0530

    Update ExpenseService.java

--- src/expense_tracker_package/ExpenseService.java
+++ src/expense_tracker_package/ExpenseService.java
@@ -1,19 +1,27 @@
 package expense_tracker_package;
 
-import java.sql.SQLException;
+import java.util.List;
 
 public class ExpenseService {
+    private ExpenseDAO expenseDAO = new ExpenseDAO();
 
-    private ExpenseDAO expenseDAO;
-
-    public ExpenseService(ExpenseDAO expenseDAO) {
-        this.expenseDAO = expenseDAO;
+    public void addExpense(Expense expense) {
+        expenseDAO.addExpense(expense);
     }
 
-    public void addExpense(String description, double amount, String date, String category, String paymentMethod) throws SQLException {
-        // Business logic or validation can be added here before calling DAO methods
-        expenseDAO.addExpense(description, amount, date, category, paymentMethod);
+    public List<Expense> getAllExpenses() {
+        return expenseDAO.getAllExpenses();
     }
 
-    // Implement other service methods (Update, Delete, etc.)
+    public Expense getExpenseById(int id) {
+        return expenseDAO.getExpenseById(id);
+    }
+
+    public void updateExpense(Expense expense) {
+        expenseDAO.updateExpense(expense);
+    }
+
+    public boolean deleteExpense(int id) {
+        return expenseDAO.deleteExpense(id);
+    }
 }
