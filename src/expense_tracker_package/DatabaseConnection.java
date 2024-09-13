c040f208aae7aac63724ebbd56458d1db2c575a8
Girija <ramagirija95@gmail.com>
2024-09-13 17:59:50 +0530

    Update DatabaseConnection.java

--- src/expense_tracker_package/DatabaseConnection.java
+++ src/expense_tracker_package/DatabaseConnection.java
@@ -5,14 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ExpenseTracker";
+    private static final String USER = "root";
+    private static final String PASSWORD = "*MySQL01Girija";
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/ExpenseTracker";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "*MySQL01Girija";
-
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-        
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database", e);
+        }
     }
-    
 }
