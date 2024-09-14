bd0325ea7d34cc268695278abb0689d60ba1f725
Girija <ramagirija95@gmail.com>
2024-09-14 11:53:57 +0530

    Update DatabaseConnection (Normal update).java

--- src/expense_tracker_package/DatabaseConnection.java
+++ src/expense_tracker_package/DatabaseConnection.java
@@ -3,7 +3,6 @@ package expense_tracker_package;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ExpenseTracker";
     private static final String USER = "root";
