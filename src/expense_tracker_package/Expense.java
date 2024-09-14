64c0c506635218d49e572b152b79cf45d03dc898
Girija <ramagirija95@gmail.com>
2024-09-14 11:51:57 +0530

    Update Expense (Included JavaFX).java

--- src/expense_tracker_package/Expense.java
+++ src/expense_tracker_package/Expense.java
@@ -1,44 +1,64 @@
 package expense_tracker_package;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Expense {
-    private int id;
-    private String description;
-    private double amount;
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty description;
+    private SimpleDoubleProperty amount;
 
     public Expense() {
+        this.id = new SimpleIntegerProperty();
+        this.description = new SimpleStringProperty();
+        this.amount = new SimpleDoubleProperty();
     }
 
     public Expense(String description, double amount) {
-        this.description = description;
-        this.amount = amount;
+        this();
+        this.description.set(description);
+        this.amount.set(amount);
     }
 
     public int getId() {
-        return id;
+        return id.get();
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id.set(id);
     }
 
     public String getDescription() {
-        return description;
+        return description.get();
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        this.description.set(description);
     }
 
     public double getAmount() {
-        return amount;
+        return amount.get();
     }
 
     public void setAmount(double amount) {
-        this.amount = amount;
+        this.amount.set(amount);
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public SimpleStringProperty descriptionProperty() {
+        return description;
+    }
+
+    public SimpleDoubleProperty amountProperty() {
+        return amount;
     }
 
     @Override
     public String toString() {
-        return "ID: " + id + ", Description: " + description + ", Amount: " + amount;
+        return "ID: " + id.get() + ", Description: " + description.get() + ", Amount: " + amount.get();
     }
 }
