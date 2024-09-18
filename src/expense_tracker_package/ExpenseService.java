73d18f7703f0e1fa764c16bd3d981a8b77cc0a3d
Girija <ramagirija95@gmail.com>
2024-09-18 18:24:03 +0530

    some changes (ExpenseService.java)

--- src/expense_tracker_package/ExpenseService.java
+++ src/expense_tracker_package/ExpenseService.java
@@ -2,23 +2,19 @@ package expense_tracker_package;
 
 import java.util.List;
 public class ExpenseService {
-    private ExpenseDAO expenseDAO = new ExpenseDAO();
+    private final ExpenseDAO expenseDAO = new ExpenseDAO();
     public void addExpense(Expense expense) {
         expenseDAO.addExpense(expense);
     }
-
     public List<Expense> getAllExpenses() {
         return expenseDAO.getAllExpenses();
     }
-
     public Expense getExpenseById(int id) {
         return expenseDAO.getExpenseById(id);
     }
-
     public void updateExpense(Expense expense) {
         expenseDAO.updateExpense(expense);
     }
-
     public boolean deleteExpense(int id) {
         return expenseDAO.deleteExpense(id);
     }
