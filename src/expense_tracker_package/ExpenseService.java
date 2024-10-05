f533b2cb8f9ee2922734ec49b2d35bdff6bd4a4b
Girija <ramagirija95@gmail.com>
2024-10-05 19:32:40 +0530

    deletion operation is corrected.
    deletion is now done using the expense details instead of expense id or index.

--- src/expense_tracker_package/ExpenseService.java
+++ src/expense_tracker_package/ExpenseService.java
@@ -1,18 +1,20 @@
 package expense_tracker_package;
 
 import java.util.List;
+
 public class ExpenseService {
     private final ExpenseDAO expenseDAO = new ExpenseDAO();
+
     public void addExpense(Expense expense) {
         expenseDAO.addExpense(expense);
     }
     public List<Expense> getAllExpenses() {
         return expenseDAO.getAllExpenses();
     }
-    public void updateExpense(Expense expense, int index) {
-        expenseDAO.updateExpense(expense, index);
+    public void updateExpense(Expense newExpense, Expense oldExpense) {
+        expenseDAO.updateExpense(newExpense, oldExpense);
     }
-    public boolean deleteExpense(int id) {
-        return expenseDAO.deleteExpense(id);
+    public boolean deleteExpense(Expense expense) {
+        return expenseDAO.deleteExpense(expense);
     }
 }
